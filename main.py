83d4a321a2ad12aae860890f937ba6c5337bf71d
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2021-11-16 06:40:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+print(student_heights)
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+total_height = 0
+for height in student_heights:
+  total_height += height
+# print(total_height)
+
+number_of_students = 0
+for student n student_heights:
+  number_of_students += 1
+
+avg_height = round(total_height / number_of_students)
+print(avg_height)
+
+
+
+
+
+
+
+
+
